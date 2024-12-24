78e33398e38b56c1f5c672f51ac9690467bd3cf2
David-Lyu <lyu.david@yahoo.com>
2024-12-24 15:40:11 -0800

    need to pass test

--- golang/remove_dupe_med.go
+++ golang/remove_dupe_med.go
@@ -2,30 +2,38 @@ func removeDuplicates(nums []int) int {
 	var count = 0
 	var index = 0
 	var hasDeleted = false
+
 	for i := 1; i < len(nums); i++ {
-		if i == len(nums)-1 {
-			break
-		}
 		//if same
 		if nums[index] == nums[i] {
 			//increase count
 			if count < 2 {
-				nums[index] = nums[i-1]
-				index++
+				// Doesn't matter since they the same
+				nums[index] = nums[i]
+				if i < len(nums)-1 {
+					index++
+				}
+			} else {
+				hasDeleted = true
 			}
 			count++
 			continue
 		} else {
-			if count > 1 {
+			if count > 1 || hasDeleted {
+				hasDeleted = true
 				nums[index] = nums[i]
 			} else {
 				nums[index] = nums[i-1]
 			}
+
+			count = 0
+			index++
+
 		}
 
-		count = 0
-		index++
-
+	}
+	if !hasDeleted {
+		return len(nums)
 	}
 	return index
 }
