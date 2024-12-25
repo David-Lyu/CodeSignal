4e49855848791d0eca755d0f1849d309d438906c
David-Lyu <lyu.david@yahoo.com>
2024-12-25 10:12:50 -0800

    try 3

--- golang/remove_dupe_med.go
+++ golang/remove_dupe_med.go
@@ -1,39 +1,74 @@
 func removeDuplicates(nums []int) int {
-	var count = 0
-	var index = 0
+	var count, index = 0, 0
 	var hasDeleted = false
 
+	if len(nums) == 1 {
+		return 1
+	}
+
 	for i := 1; i < len(nums); i++ {
-		//if same
 		if nums[index] == nums[i] {
-			//increase count
-			if count < 2 {
-				// Doesn't matter since they the same
-				nums[index] = nums[i]
-				if i < len(nums)-1 {
+			count++
+			if count >= 2 && !hasDeleted {
+				if count == 2 {
 					index++
 				}
-			} else {
-				hasDeleted = true
+				hasDelete = true
+				continue
 			}
-			count++
-			continue
-		} else {
-			if count > 1 || hasDeleted {
-				hasDeleted = true
-				nums[index] = nums[i]
-			} else {
-				nums[index] = nums[i-1]
-			}
-
-			count = 0
 			index++
-
+		} else {
+			count = 0
+			if hasDeleted {
+				nums[index] = nums[i]
+			}
+			index++
 		}
-
 	}
 	if !hasDeleted {
 		return len(nums)
 	}
 	return index
+}
+
+func old_1() int {
+	//var count = 0
+	//var index = 0
+	//var hasDeleted = false
+
+	//for i := 1; i < len(nums); i++ {
+	//	//if same
+	//	if nums[index] == nums[i] {
+	//		//increase count
+	//		if count < 2 {
+	//			// Doesn't matter since they the same
+	//			nums[index] = nums[i]
+	//			if i < len(nums)-1 {
+	//				index++
+	//			}
+	//		}
+	//		count++
+	//		if count >= 2 {
+	//			hasDeleted = true
+	//		}
+	//		continue
+	//	} else {
+	//		if count > 1 || hasDeleted {
+	//			hasDeleted = true
+	//			nums[index] = nums[i]
+	//		} else {
+	//			nums[index] = nums[i-1]
+	//		}
+
+	//		count = 0
+	//		index++
+
+	//	}
+
+	//}
+	//if !hasDeleted {
+	//	return len(nums)
+	//}
+	//return index
+
 }
