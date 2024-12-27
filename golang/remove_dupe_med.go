33f6e7946bbf5f3c7457e6f00ec7ab01d1e43294
David-Lyu <lyu.david@yahoo.com>
2024-12-26 21:34:01 -0800

    found

--- golang/remove_dupe_med.go
+++ golang/remove_dupe_med.go
@@ -1,119 +1,13 @@
 func removeDuplicates(nums []int) int {
-	var count, index = 0, 0
-	var lastNum = nums[0]
-	var hasDeleted, noDelete = false, true
-
-	if len(nums) == 1 {
-		return 1
-	}
-
-	for i := 1; i < len(nums); i++ {
-		if nums[i] == lastNum {
-			count++
-			if i == len(nums) {
-				nums[index] = nums[i]
-			}
-			if count < 2 {
-				index++
-				continue
-			}
-
-			hasDeleted = true
-			noDelete = false
-		} else {
-			count = 0
-			lastNum = nums[i]
-			if noDelete {
-				index++
-				continue
-			}
-			if hasDeleted {
-				hasDeleted = false
-				index++
-			}
-
-			nums[index] = lastNum
-			index++
-		}
-	}
-
-	if noDelete {
+	if len(nums) < 2 {
 		return len(nums)
 	}
-	return index
-}
-
-func old_2() int {
-	var count, index = 0, 0
-	var hasDeleted = false
-
-	if len(nums) == 1 {
-		return 1
-	}
-
-	for i := 1; i < len(nums); i++ {
-		if nums[index] == nums[i] {
-			count++
-			if count >= 2 && !hasDeleted {
-				if count == 2 {
-					index++
-				}
-				hasDelete = true
-				continue
-			}
-			index++
-		} else {
-			count = 0
-			if hasDeleted {
-				nums[index] = nums[i]
-			}
-			index++
+	j := 2
+	for i := 2; i < len(nums); i++ {
+		if nums[i] != nums[j-2] {
+			nums[j] = nums[i]
+			j++
 		}
 	}
-	if !hasDeleted {
-		return len(nums)
-	}
-	return index
-
-}
-func old_1() int {
-	//var count = 0
-	//var index = 0
-	//var hasDeleted = false
-
-	//for i := 1; i < len(nums); i++ {
-	//	//if same
-	//	if nums[index] == nums[i] {
-	//		//increase count
-	//		if count < 2 {
-	//			// Doesn't matter since they the same
-	//			nums[index] = nums[i]
-	//			if i < len(nums)-1 {
-	//				index++
-	//			}
-	//		}
-	//		count++
-	//		if count >= 2 {
-	//			hasDeleted = true
-	//		}
-	//		continue
-	//	} else {
-	//		if count > 1 || hasDeleted {
-	//			hasDeleted = true
-	//			nums[index] = nums[i]
-	//		} else {
-	//			nums[index] = nums[i-1]
-	//		}
-
-	//		count = 0
-	//		index++
-
-	//	}
-
-	//}
-	//if !hasDeleted {
-	//	return len(nums)
-	//}
-	//return index
-
+	return j
 }
