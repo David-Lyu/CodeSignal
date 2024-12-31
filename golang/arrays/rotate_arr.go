f7c7ed5bb0bacd351f0032cf5ece54edb8eac379
David-Lyu <lyu.david@yahoo.com>
2024-12-30 20:26:26 -0800

    reformated logic

--- golang/arrays/rotate_arr.go
+++ golang/arrays/rotate_arr.go
@@ -1,6 +1,15 @@
 func rotate(nums []int, k int) {
+	temp := 0
+	for i:= len(nums)-1; i >= k; i-- {
+		temp = nums[i]
+		nums[i] = nums[i-k]
+		nums[i-k] = temp
+	} 
 
-	var temp = 0
+}
+
+func old1(nums []int,kint) {
+var temp = 0
 	if k >= len(nums)/2.0 {
 		for i := len(nums) - 1; k > 0; i-- {
 			temp = nums[i]
@@ -21,5 +30,4 @@ func rotate(nums []int, k int) {
 			}
 		}
 	}
-
 }
