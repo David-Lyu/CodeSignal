f5a7ba4d6e73380ed8421417548e6ddfc9d22405
David-Lyu <lyu.david@yahoo.com>
2024-12-30 17:30:02 -0800

    update test

--- golang/arrays/rotate_arr.go
+++ golang/arrays/rotate_arr.go
@@ -1,9 +1,25 @@
 func rotate(nums []int, k int) {
 
 	var temp = 0
-	for i := len(nums) - 1; i >= len(nums)-k-1; i-- {
-		temp = nums[i]
-		nums[i] = nums[i-k]
-		nums[i-k] = temp
+	if k >= len(nums)/2.0 {
+		for i := len(nums) - 1; k > 0; i-- {
+			temp = nums[i]
+			nums[i] = nums[k-1]
+			nums[k-1] = temp
+			k--
+		}
+	} else {
+		//slice array
+		var size = len(nums)
+		for i := 0; i < size-1; i++ {
+			if k-i-1 >= 0 {
+				temp = nums[size-i-1]
+				nums[size-i-1] = nums[k]
+				nums[k] = temp
+			} else {
+
+			}
+		}
 	}
+
 }
