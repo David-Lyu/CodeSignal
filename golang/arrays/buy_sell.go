707019833f021d86d244c3ad865cf7d7b4f2e371
David-Lyu <lyu.david@yahoo.com>
2025-01-03 17:49:35 -0800

    new buy_sell

---
+++ golang/arrays/buy_sell.go
@@ -0,0 +1,15 @@
+
+func maxProfit(prices []int) int {
+	type pStruct struct {
+		hightest int
+		loweset  int
+		index    int
+	}
+
+	var store = pStruct{lowest = -1}
+	var size = len(prices)
+
+	for i := size - 1; i >= 0; i++ {
+
+	}
+}
